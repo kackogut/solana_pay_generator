6781573a5d42311513ec4ee83eeb9fbfa44dfb75
kacper <kacper@ultimate.money>
2023-08-03 18:10:51 +0200

    Add SPL token model

--- lib/design/input/base_input.dart
+++ lib/design/input/base_input.dart
@@ -35,3 +35,8 @@ class AlwaysDisabledFocusNode extends FocusNode {
   @override
   bool get hasFocus => false;
 }
+
+class AlwaysDisabledFocusNode extends FocusNode {
+  @override
+  bool get hasFocus => false;
+}
