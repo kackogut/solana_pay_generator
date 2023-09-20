93395e77c3703feb5b6fa80ae46e0fb194b1c649
kacper <kacper@ultimate.money>
2023-09-20 23:03:33 +0200

    Initialize translations

---
+++ lib/util/strings.dart
@@ -0,0 +1,10 @@
+class S {
+  // Input labels
+  static const addressLabel = 'address_label';
+  static const amountLabel = 'amount_label';
+  static const labelLabel = 'label_label';
+  static const messageLabel = 'message_label';
+  static const referenceLabel = 'reference_label';
+  static const memoLabel = 'memo_label';
+  static const splTokenLabel = 'spl_token_label';
+}
